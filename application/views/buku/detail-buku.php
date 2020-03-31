5c5408c65dbdbebc472bcea105c6ca824af9c2b9
nirwanmaulanamustafa <51256933+nirwanmaulanamustafa@users.noreply.github.com>
2020-03-31 21:15:27 +0700

    Add files via upload

--- application/views/buku/detail-buku.php
+++ application/views/buku/detail-buku.php
@@ -1,6 +1,4 @@
 <div class="x_panel" align="center">
-
-
     <div class="x_content">
         <div class="row">
             <div class="col-sm-3 col-md-3">
@@ -9,7 +7,6 @@
                         style="max-width:100%; max-height: 100%; height: 150px; width: 120px">
                     <div class="caption">
                         <h5 style="min-height:40px;" align="center"><?= $pengarang ?></h5>
-
                         <center>
                             <table class="table table-stripped">
                                 <tr>
@@ -43,8 +40,8 @@
                             </table>
                         </center>
                         <p> <a class="btn btn-outline-primary fas fw fa-shopping-cart"
-                                href="<?= base_url('booking/tambahBooking/' . $id); ?>"> Booking</a> 
-                                <span class="btn btn-outline-secondary fas fw fa-reply" onclick="window.history.go(-1)">
+                                href="<?= base_url('booking/tambahBooking/' . $id); ?>"> Booking</a> <span
+                                class="btn btn-outline-secondary fas fw fa-reply" onclick="window.history.go(-1)">
                                 Kembali</span> </p>
                     </div>
                 </div>
