f6e1c2375364e81942dad2bed83a099778927975
nirwanmaulanamustafa <51256933+nirwanmaulanamustafa@users.noreply.github.com>
2020-04-02 09:56:05 +0700

    Add files via upload

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,7 +5,7 @@ class Home extends CI_Controller{
     function __construct()
     {
         parent::__construct();
-        $this->load->model(['ModelBuku', 'ModelUser']);
+        $this->load->model(['ModelBuku', 'ModelUser', 'ModelBooking']);
     }
 
     public function index()
@@ -22,14 +22,14 @@ class Home extends CI_Controller{
             $data['user'] = $user['nama'];
             $this->load->view('templates/templates-user/header', $data); 
             $this->load->view('buku/daftarbuku', $data);
-            //$this->load->view('templates/templates-user/modal'); 
+            $this->load->view('templates/templates-user/modal'); 
             $this->load->view('templates/templates-user/footer', $data);
         }else 
         { 
             $data['user'] = 'Pengunjung'; 
             $this->load->view('templates/templates-user/header', $data); 
             $this->load->view('buku/daftarbuku', $data); 
-            //$this->load->view('templates/templates-user/modal'); 
+            $this->load->view('templates/templates-user/modal'); 
             $this->load->view('templates/templates-user/footer', $data); 
         }
     }
